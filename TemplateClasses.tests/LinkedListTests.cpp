7e0c84f7302dd4dda575bba1b8774aace13f4133
ErrorX <felix@edenborgh.se>
2018-12-05 14:56:23 +0100

    Linkedlist

--- TemplateClasses.tests/LinkedListTests.cpp
+++ TemplateClasses.tests/LinkedListTests.cpp
@@ -1,6 +1,20 @@
 #include "gtest/gtest.h"
+#include "../TemplateClasses/LinkedList.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
+
+TEST(LinkedListtrests, AddNode) {
+
+	LinkedList list;
+	list.AddNode(3);
+	list.AddNode(5);
+	list.AddNode(7);
+	list.AddNode(2);
+	cout << n << endl;
+
+
   EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+  EXPECT_TRUE(3572);
 }
--- TemplateClasses/LinkedList.h
+++ TemplateClasses/LinkedList.h
@@ -1,8 +1,80 @@
 #pragma once
-class LinkedList
-{
-public:
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+template <class T>
+class LinkedList{
+private:
+	typedef struct Node {
+		int data;
+		struct Node *next;
+	}* Nodeptr;
+	typedef struct Node* Nodeptr;
+
+	Nodeptr head;
+	Nodeptr curr;
+	Nodeptr temp;
+	template <class T>
+	LinkedList() {
+		head = NULL;
+		curr = NULL;
+		temp = NULL;
+	}
+public: 
 	LinkedList();
-	~LinkedList();
+	template <class T>
+	AddNode(T AddData) {
+		Nodeptr n = new Node;
+		n->next = NULL;
+		n->data = AddData;
+
+		if (head != NULL) {
+			curr = head;
+			while (curr->next != NULL) {
+				curr = curr->next;
+			}
+			curr->next = n;
+		}
+		else {
+			head = n;
+		}
+	}
+	template<class T>
+	DeleteNode(T DelData) {
+		Nodeptr delPtr = NULL;
+		temp = head;
+		curr = head;
+		while (curr != NULL && curr->data != DelData) {
+			temp = curr;
+			curr = curr->next;
+		}
+		if (curr == NULL) {
+			cout << "DelData " << "was not in the list \n" << endl;
+			delete delPtr;
+		}
+		else {
+			delPtr = curr;
+			curr = curr->next;
+			temp->next = curr;
+			delete delPtr;
+			cout << "The value " << DelData << "was deleted \n" << endl;
+
+		}
+	}
+	template <class T>
+	PrintList() {
+		curr = head;
+		while (curr != NULL) {
+			cout << curr->data << endl;
+			curr = curr->next;
+		}
+	}
+
+
+	
+
 };
 
