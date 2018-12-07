551b9a65c36e79c1f16010fd7747f3fb9b3f72e7
ErrorX <felix@edenborgh.se>
2018-12-07 12:25:50 +0100

    Har Hållit på med tests, dem är inte 100% klara ännu.

--- TemplateClasses.tests/LinkedListTests.cpp
+++ TemplateClasses.tests/LinkedListTests.cpp
@@ -21,3 +21,59 @@ TEST(LinkedListTests, WhenPushFrontHeadsNextShouldBeOldHead)
 
 	EXPECT_EQ(2, list->Front()->getNext()->getData());
 }
+
+TEST(LinkedListTests, WhenPushFrontShouldInputFivenumbersToList) {
+
+	LinkedList<int>* List = new LinkedList<int>();
+	List->PushFront(0);
+	List->PushFront(1);
+	List->PushFront(2);
+	List->PushFront(3);
+	List->PushFront(4);
+
+	EXPECT_TRUE(true);
+}
+
+TEST(LinkedListTests, ShouldPushBackItemsToTheBackOfTheList) {
+	LinkedList<int>* List = new LinkedList<int>();
+	List->PushBack(8);
+	List->PushBack(3);
+	List->PushBack(9);
+	
+	EXPECT_TRUE(true);
+
+}
+
+TEST(LinkedListTests, CanitWorkWithChars) {
+	LinkedList<const char>* List = new LinkedList<const char>();
+
+	//List->PushFront("Hej");
+
+	
+
+}
+
+TEST(LinkedListtests, CheckSize) {
+	LinkedList<int>* List = new LinkedList<int>();
+	List->PushBack(9);
+	List->PushBack(3);
+	List->PushBack(2);
+	
+	EXPECT_EQ(3, List<int>.size() == 3);
+}
+
+TEST(LinkedListTests, TestIfTheStringsAreEqual) {
+	LinkedList<std::string>* List = new LinkedList<std::string>();
+	std::string hej = "HEJ";
+	List->PushFront(hej);
+	
+	EXPECT_EQ(hej, List->PopFront());
+
+}
+
+TEST(LinkedListTests, TestIfTheStringsAreEqual) {
+	std::string hej = "HEJ";
+
+	EXPECT_STREQ("hej", "hej");
+
+}
