59e28513116f649ff1ab00b1ea5473fd1cb18f5b
Daniel <danielaxel@hotmail.com>
2018-12-07 11:31:03 +0100

    Worked on DoubleLinkedList.h

--- TemplateClasses/DoubleLinkedList.h
+++ TemplateClasses/DoubleLinkedList.h
@@ -7,30 +7,39 @@ public:
 	class Node {
 	private:
 		T data;
-		DoubleLinkedList<T>::Node* next;
+		DoubleLinkedList<T>::Node *next;
+		DoubleLinkedList<T>::Node *previous;
+
 	public:
 		Node() {};
 		Node(T data)
 		{
 			this->data = data;
 			this->next = NULL;
+			this->previous = NULL;
 		};
 		~Node() {};
-		void setData(T data) { this->data = data; };
-		T getData() { return data; };
-		void setNext(DoubleLinkedList<T>::Node* next) { this->next = next; };
-		DoubleLinkedList<T>::Node* getNext() { return next; };
+		void setData(T data) { this->data = data; }
+		T getData() { return this->data; }
+		void setNext(DoubleLinkedList<T>::Node *next) { this->next = next; }
+		void setPrevious(DoubleLinkedList<T>::Node *previous) { this->previous = previous; }
+		DoubleLinkedList<T>::Node* getNext() { return next; }
+		DoubleLinkedList<T>::Node* getPrevious() { return previous; }
 	};
+
 private:
-	DoubleLinkedList<T>::Node* head, *tail;
-	void setHead(DoubleLinkedList<T>::Node* head) { this->head = head; };
-	void setTail(DoubleLinkedList<T>::Node* tail) { this->tail = tail; };
+	DoubleLinkedList<T>::Node *head, *tail;
+	void setHead(DoubleLinkedList<T>::Node* head) { this->head = head; }
+	void setTail(DoubleLinkedList<T>::Node* tail) { this->tail = tail; }
+	DoubleLinkedList<T>::Node* GetNode(T data)
+	{}
+
 public:
-	DoubleLinkedList() : head{ NULL }, tail{ NULL } {};
-	~DoubleLinkedListhead() {};
-	DoubleLinkedList<T>::Node* Front() { return head; };
-	DoubleLinkedList<T>::Node* Back() { return tail; };
-	void PushFront(const T& data)
+	DoubleLinkedList() : head{ NULL }, tail{ NULL } {}
+	~DoubleLinkedList() {}
+	T Front() { return this->head->getData(); }
+	T Back() { return this->tail->getData(); }
+	void PushFront(T data)
 	{
 		DoubleLinkedList<T>::Node* node = new DoubleLinkedList<T>::Node(data);
 
@@ -41,29 +50,76 @@ public:
 		}
 		else
 		{
+			this->head->setPrevious(node);
 			node->setNext(head);
-			head = node;
+			this->head = node;
 		}
-	};
-	void PushBack(const T& data)
+	}
+	void PushBack(T data)
 	{
-	};
-	DoubleLinkedList<T>::Node* PopFront()
+		DoubleLinkedList<T>::Node* node = new DoubleLinkedList<T>::Node(data);
+		node->setPrevious(this->tail);
+		this->tail->setNext(node);
+		this->tail = node;
+	}
+	T PopFront()
 	{
-	};
-	DoubleLinkedList<T>::Node* PopBack()
+		DoubleLinkedList<T>::Node* node = this->head;
+		T data = node->getData();
+		if (node->next != NULL)
+		{
+			setHead(this->head->next);
+			this->head->setPrevious(NULL);
+		}
+		else
+		{
+			this->head = NULL;
+			this->tail = NULL;
+		}
+		delete node;
+		return data;
+	}
+	T PopBack()
 	{
-	};
-	int Length()
+		DoubleLinkedList<T>::Node* node = this->tail;
+		T data = node->getData();
+		if (node->previous != NULL)
+		{
+			setTail(this->tail->previous);
+			this->tail->setNext(NULL);
+		}
+		else
+		{
+			this->head = NULL;
+			this->tail = NULL;
+		}
+		delete node;
+		return data;
+	}
+	int Size()
 	{
-	};
-	void Insert()
+		if (this->head == NULL)
+		{
+			return 0;
+		}
+		DoubleLinkedList<T>::Node* temp = this->head;
+		int size = 0;
+		while (temp != NULL)
+		{
+			temp = temp->next;
+			size++;
+		}
+		return size;
+	}
+	T Get(int index)
 	{
-	};
-	void Delete()
+
+	}
+	void Insert(int index)
 	{
-	};
-	DoubleLinkedList<T>::Node* GetNode(T data)
+	}
+	void Delete(int index)
 	{
-	};
+	}
+	
 };
